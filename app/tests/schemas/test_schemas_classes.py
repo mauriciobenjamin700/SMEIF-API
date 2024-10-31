b0966d02bc21e5d82bc4fc9c17443501d74f0ed9
Mauricio Benjamin <mauriciobenjamin700@gmail.com>
2024-10-31 08:32:52 -0300

    tests: bugs in ClassRequest tests

--- app/tests/schemas/test_schemas_classes.py
+++ app/tests/schemas/test_schemas_classes.py
@@ -2,6 +2,12 @@ from fastapi import HTTPException
 from pytest import raises
 
 
+from constants.base import ERROR_INVALID_CPF
+from constants.classes import(
+  ERROR_CLASSES_REQUIRED_FIELD_NAME,
+  ERROR_CLASSES_REQUIRED_FIELD_ROOM,
+  ERROR_CLASSES_REQUIRED_FIELD_TEACHER_CPF  
+)
 from schemas.classes import (
     ClassRequest,
     ClassEventRequest,
@@ -21,3 +27,52 @@ def test_ClassRequest_success():
     assert schema.name == data["name"]
     assert schema.room == data["room"]
     assert schema.teacher_cpf == unformat_cpf(data["teacher_cpf"])
+
+
+def test_ClassRequest_fail_no_name():
+    data = {
+        "room": "Sala 01",
+        "teacher_cpf": "123.456.789-00"
+    }
+    with raises(HTTPException) as e:
+        ClassRequest(**data)
+    
+    e.value.status_code == 400
+    e.value.detail == ERROR_CLASSES_REQUIRED_FIELD_NAME
+
+
+def test_ClassRequest_fail_no_room():
+    data = {
+        "name": "Matemática",
+        "teacher_cpf": "123.456.789-00"
+    }
+    with raises(HTTPException) as e:
+        ClassRequest(**data)
+    
+    e.value.status_code == 400
+    e.value.detail == ERROR_CLASSES_REQUIRED_FIELD_ROOM
+
+
+def test_classRequest_fail_no_teacher_cpf():
+    data = {
+        "name": "Matemática",
+        "room": "Sala 01"
+    }
+    with raises(HTTPException) as e:
+        ClassRequest(**data)
+    
+    e.value.status_code == 400
+    e.value.detail == ERROR_CLASSES_REQUIRED_FIELD_TEACHER_CPF
+
+
+def test_ClassRequest_fail_invalid_teacher_cpf():
+    data = {
+        "name": "Matemática",
+        "room": "Sala 01",
+        "teacher_cpf": "123.456.789-011"
+    }
+    with raises(HTTPException) as e:
+        ClassRequest(**data)
+    
+    e.value.status_code == 400
+    e.value.detail == ERROR_INVALID_CPF
